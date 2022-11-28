7c345b931dab7a470978ccf781a787d7f00d8d4c
JuanBautistaEsteban <jbesteban.personal@gmail.com>
2022-11-28 13:33:39 +0100

    Comenzando con el proyecto

---
+++ TimeTabling2.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeTabling2.Web.Startup))]
+namespace TimeTabling2.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
